e333b28c80d6744327d019524bd804f10dc7955a
lauwarm <10065855+lauwarm@users.noreply.github.com>
2021-01-20 18:25:56 +0100

    Initial Commit

---
+++ SimpleComposingComplexInterfaces/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SimpleComposingComplexInterfaces
+//
+//  Created by Fabian on 20.01.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SimpleComposingComplexInterfaces/SimpleComposingComplexInterfacesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleComposingComplexInterfacesApp.swift
+//  SimpleComposingComplexInterfaces
+//
+//  Created by Fabian on 20.01.21.
+//
+
+import SwiftUI
+
+@main
+struct SimpleComposingComplexInterfacesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
